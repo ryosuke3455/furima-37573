cce16816c93867012633c4c17db9ca5da9720d94
ryosuke3455 <ryosuke.m2ds7@softbank.ne.jp>
2022-02-22 12:48:22 +0900

    新規登録機能の実装

--- app/models/bought_rec.rb
+++
@@ -1,2 +0,0 @@
-class BoughtRec < ApplicationRecord
-end
--- db/migrate/20220221080114_create_bought_recs.rb
+++
@@ -1,8 +0,0 @@
-class CreateBoughtRecs < ActiveRecord::Migration[6.0]
-  def change
-    create_table :bought_recs do |t|
-
-      t.timestamps
-    end
-  end
-end
