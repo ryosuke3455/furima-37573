4729376567dae24908139ed76a0108593d2dce68
ryosuke3455 <ryosuke.m2ds7@softbank.ne.jp>
2022-03-01 19:06:00 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
 
   def new
+    @item = Item.new
   end
-  def destroy
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :category_id, :price, :quality_id, :explanation, 
+      :prefecture_id, :delivery_charge_id, :posting_id, :images).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, numericality: { other_than: 0 }
+  validates :quality_id, numericality: { other_than: 0 }
+  validates :delivery_charge_id, numericality: { other_than: 0 }
+  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :posting_id, numericality: { other_than: 0 }
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+
+  belongs_to :user
+  has_one :bought_rec
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :quality, :delivery_charge, :prefecture, :posting
+end
