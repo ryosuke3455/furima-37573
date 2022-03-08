a5b7fc147ac4efdfe3fdae12193229041e6821a1
ryosuke3455 <ryosuke.m2ds7@softbank.ne.jp>
2022-03-08 20:35:05 +0900

    検証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password,
       :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
